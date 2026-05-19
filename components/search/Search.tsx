7b45d98fbb044ffd0043aeb0b32fe94dcc654fbd
GilvanZ <gilvan@example.com>
2026-05-19 12:31:12 +0100

    fix onChange search.tsx

--- components/search/Search.tsx
+++ components/search/Search.tsx
@@ -2,8 +2,8 @@ import { ValueOf } from 'next/dist/shared/lib/constants';
 import React from 'react'
 
 interface SearchProps {
-  value?: string
-  onChange?: (value: string) => void
+  value: string
+  onChange: (value: string) => void
 }
 
 export default function Search({value, onChange}:SearchProps) {
